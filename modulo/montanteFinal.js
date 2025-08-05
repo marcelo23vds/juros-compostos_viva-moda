d820e346e766d20077ffd961135b2c7c3ef070ea
MARCELO VIEIRA DA SILVA <25132416@CFP127ALUNO.LOCAL>
2025-08-05 11:18:02 -0300

    adicionando comentarios

---
+++ modulo/montanteFinal.js
@@ -0,0 +1,22 @@
+/********************************************************************
+ * Objetivo: Funções para validar entradas de dados, calcular o montante final e exibir o resultado
+ * Autor: Marcelo Vieira
+ * Data: 05/08/2025
+ * Versão: 1.0
+********************************************************************/
+
+//funcoes para validar as entradas
+
+    //é possivel que seja 2 ou mais funções para isso... mas acho que da pra fazer em 1 só
+
+//funcoes para converter (caso seja necessario)
+
+    //verificar, acredito que seja possivel com 1 também
+
+//funcao para calcular o montante
+
+    //apenas uma recebendo 4 parametros
+
+//funcao para exibir o resultado ao usuario
+
+    //apenas uma recebendo 5 parametros (provavelmente)
